e5b6f91c576be59bcf3dcf8a0703303f0728420d
cqm <mtdxc@126.com>
2022-10-12 15:27:56 +0800

    对齐代码

--- examples/dlna/UpnpRender.h
+++ examples/dlna/UpnpRender.h
@@ -1,84 +1,80 @@
 ﻿#pragma once
+#include <memory>
+#include <string>
+#include <functional>
 #include "UpnpServer.h"
-struct AVPositionInfo {
-  float trackDuration = 0.0f;
-  float absTime = 0.0f;
-  float relTime = 0.0f;
-};
 
-struct TransportInfo {
-  std::string currentTransportState, currentTransportStatus;
-  float currentSpeed;
-};
-
-class UpnpRender
+class UpnpRender : public std::enable_shared_from_this<UpnpRender>
 {
   Device::Ptr model_;
   std::string url_;
   float duration_;
+  float speed_ = 1.0f;
 public:
-  const char* devId() const;
+  typedef std::shared_ptr<UpnpRender> Ptr;
   UpnpRender(Device::Ptr dev) :model_(dev) {}
-  
-  typedef std::function<void(int, std::string)> RpcCB;
+  ~UpnpRender();
+
+  const char* devId() const;
+  float duration() const { return duration_; }
+  float speed() const { return speed_; }
+
   /**
    设置投屏地址
    @param urlStr 视频url
    */
   int setAVTransportURL(const char* urlStr, RpcCB cb = nullptr);
   std::string getURL() const { return url_; }
-  float getDuration() const { return duration_; }
   /**
    播放
    */
-   int play(RpcCB cb = nullptr);
+  int play(float speed = 1.0f, RpcCB cb = nullptr);
 
   /**
    暂停
    */
-   int pause(RpcCB cb = nullptr);
+  int pause(RpcCB cb = nullptr);
 
   /**
    结束
    */
-   int stop(RpcCB cb = nullptr);
+  int stop(RpcCB cb = nullptr);
 
 
   /**
    跳转进度
    @param relTime 进度时间(单位秒)
    */
-   int seek(float relTime, RpcCB cb = nullptr);
+  int seek(float relTime, RpcCB cb = nullptr);
 
   /**
    跳转至特定进度或视频
    @param target 目标值，可以是 00:02:21 格式的进度或者整数的 TRACK_NR。
    @param unit   REL_TIME（跳转到某个进度）或 TRACK_NR（跳转到某个视频）。
-   */
-   int seekToTarget(const char* target, const char* unit, RpcCB cb = nullptr);
+  */
+  int seekToTarget(const char* target, const char* unit, RpcCB cb);
 
   /**
    获取播放进度,可通过协议回调使用
    */
-   int getPositionInfo(std::function<void(int, AVPositionInfo)> cb);
+  int getPositionInfo(std::function<void(int, AVPositionInfo)> cb);
 
   /**
    获取播放状态,可通过协议回调使用
    */
-   int getTransportInfo(std::function<void(int, TransportInfo)> cb);
+  int getTransportInfo(std::function<void(int, TransportInfo)> cb);
 
   /**
    获取音频,可通过协议回调使用
    */
-   int getVolume(std::function<void(int, int)> cb);
-   // 0-100
-   int setVolume(int value, RpcCB cb = nullptr);
+  int getVolume(std::function<void(int, int)> cb);
+  // 0-100
+  int setVolume(int value, RpcCB cb = nullptr);
 
   /**
    设置音频值
    @param value 值—>整数
    */
-   int setVolumeWith(const char* value, RpcCB cb = nullptr);
+  int setVolumeWith(const char* value, RpcCB cb = nullptr);
 
 };
-
--- examples/dlna/UpnpServer.h
+++ examples/dlna/UpnpServer.h
@@ -1,13 +1,14 @@
-﻿#pragma once
+#pragma once
 #include <map>
+#include <memory>
 #include <string>
 #include <memory>
 #include "UdpClient.h"
 #include "AsyncHttpClient.h"
 #include "HttpServer.h"
-extern const char* ssdpAddres;
-extern const unsigned short ssdpPort;
 
+
+class UpnpRender;
 enum UpnpServiceType {
   USInvalid = -1,
   USAVTransport,
@@ -43,54 +44,104 @@ struct Device {
 };
 typedef std::map<std::string, Device::Ptr> MapDevices;
 
+struct AVPositionInfo {
+  float trackDuration;
+  float absTime;
+  float relTime;
+  AVPositionInfo() { trackDuration = absTime = relTime = 0.0f; }
+};
+
+struct TransportInfo {
+  char state[32];
+  char status[32];
+  float speed;
+  TransportInfo();
+  void setState(const char* v);
+  void setStatus(const char* v);
+};
+typedef std::function<void(int, std::string)> RpcCB;
+
 class UpnpListener {
 public:
   virtual void upnpSearchChangeWithResults(const MapDevices& devs) = 0;
-  // 所有UPnPAction调用时都会返回一个唯一id, 可通过hook此方法来获取所有soap调用返回
+  // 调用UPnPAction时,会返回一个id, 可通过hook此方法来获取所有soap调用的返回值
   virtual void unppActionResponse(int id, int code, const std::map<std::string, std::string>& args) {}
 };
 
 class Upnp
 {
-  MapDevices _devices; //已经发现的设备
+  // 打开的Render
+  std::map<std::string, std::shared_ptr<UpnpRender>> _renders;
+  MapDevices _devices; //发现的设备
   hv::UdpClient _socket;
-  std::shared_ptr<hv::AsyncHttpClient> _http_client;
   void onUdpRecv(char* buf, int size);
+
+  hv::HttpServer _http_server;
+  hv::HttpService _http_service;
+  std::shared_ptr<hv::AsyncHttpClient> _http_client;
+  void startHttp();
 
   void loadDeviceWithLocation(std::string loc, std::string usn);
   void addDevice(Device::Ptr device);
   void delDevice(const std::string& usn);
   void onChange();
-  UpnpListener* _listener = nullptr;
+
+  std::list<UpnpListener*> _listeners;
+
+  char url_prefix_[128];
+  // http文件映射
+  std::map<std::string, std::string> file_maps_;
+
   Upnp() = default;
+  Device::Ptr getDevice(const char* usn);
+  std::shared_ptr<UpnpRender> getRender(const char* usn);
+  void detectLocalIP();
 public:
-  void setListener(UpnpListener* l) { _listener = l; }
-  UpnpListener* getListener() const {return _listener;}
+  const char* getUrlPrefix();
+  // return local_uri_ + "/" + loc
+  std::string getUrl(const char* path);
+
+  // 本地文件共享
+  std::string setFile(const char* path, const char* loc = nullptr);
+  // 保证旧版兼容
+  std::string setCurFile(const char* path) {
+    return setFile(path, "/media");
+  }
+  std::string getCurUrl() {
+    return getUrl("/media");
+  }
+
+  void addListener(UpnpListener* l);
+  void delListener(UpnpListener* l);
+  // used for UpnpRender
+  const std::list<UpnpListener*>& getListeners() { return _listeners; }
+
   static Upnp* Instance();
   ~Upnp();
 
   hv::EventLoopPtr loop() { return _socket.loop(); }
   hv::AsyncHttpClient* httpClient() { return _http_client.get(); }
+
   void start();
-
-  void startHttp();
-
   void stop();
+
+  // 搜索设备
   void search();
+  // 返回设备列表
+  const MapDevices& getDevices() { return _devices; }
 
-  MapDevices getDevices() { return _devices; }
-  Device::Ptr getDevice(const std::string& usn);
-
-  void setCurFile(const std::string& path) { _cur_file = path; }
-  std::string getCurFile() const { return _cur_file; }
-  std::string getCurUrl() const;
-
-  void detectLocalIP();
-
-private:
-  std::string _cur_file;
-  hv::HttpServer _http_server;
-  hv::HttpService _http_service;
-  std::string local_ip;
+  // 代理 AVTransport API
+  int openUrl(const char* id, const char* url, RpcCB cb = nullptr);
+  int close(const char* id, RpcCB cb = nullptr);
+  int pause(const char* id, RpcCB cb = nullptr);
+  int resume(const char* id, RpcCB cb = nullptr);
+  int seek(const char* id, float val, RpcCB cb = nullptr);
+  int getPosition(const char* id, std::function<void(int, AVPositionInfo)> cb = nullptr);
+  int getInfo(const char* id, std::function<void(int, TransportInfo)> cb = nullptr);
+  int setSpeed(const char* id, float speed, RpcCB cb = nullptr);
+  float getSpeed(const char* id);
+  float getDuration(const char* id);
+  // RenderingControl api
+  int setVolume(const char* id, int val, RpcCB cb = nullptr);
+  int getVolume(const char* id, std::function<void(int, int)> cb = nullptr);
 };
-
