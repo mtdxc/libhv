41320af9a3d97278ca0174112d62cb3f5b191e76
cqm <mtdxc@126.com>
2022-10-21 09:22:48 +0800

    支持将HttpServer绑定到任意端口

--- http/server/HttpServer.cpp
+++ http/server/HttpServer.cpp
@@ -173,7 +173,8 @@ static void loop_thread(void* userdata) {
 int http_server_run(http_server_t* server, int wait) {
     // http_port
     if (server->port > 0) {
-        server->listenfd[0] = Listen(server->port, server->host);
+        if (server->listenfd[0] < 0)
+            server->listenfd[0] = Listen(server->port, server->host);
         if (server->listenfd[0] < 0) return server->listenfd[0];
         hlogi("http server listening on %s:%d", server->host, server->port);
     }
