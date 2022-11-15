ef62b64fdc497d692e27f79dba9c0ee3e1edae79
cqm <mtdxc@126.com>
2022-11-15 15:21:17 +0800

    解析错误码，并修复主程序提示

--- examples/dlna/main.cpp
+++ examples/dlna/main.cpp
@@ -32,7 +32,8 @@ int main(int argc, char* argv[]) {
   upnp->start();
   upnp->addListener(&listener);
   upnp->search(USAVTransport);
-  std::string cur_dev;
+  std::shared_ptr<UpnpRender> render;
+
   std::string line;
   while (getline(std::cin, line)) {
     auto cmds = hv::split(line, ' ');
@@ -54,8 +55,8 @@ int main(int argc, char* argv[]) {
       auto devs = upnp->getDevices();
       PrintDevices(devs);
     }
-    else if (cmd == "play" || cmd == "p") {
-      if (cmds.size() < 3) continue;
+    else if (cmd == "o" || cmd == "open" || cmd == "opensub") {
+      if (cmds.size() < 2) continue;
       auto devs = upnp->getDevices();
       int pos = atoi(cmds[1].c_str());
       if (pos < 0 || pos >= devs.size()) {
@@ -67,57 +68,28 @@ int main(int argc, char* argv[]) {
         it++;
         pos--;
       }
-      if (cur_dev.length())
-        upnp->close(cur_dev.c_str());
-      cur_dev = it->first;
-      auto file = cmds[2];
-      if (-1 == file.find("://")) {
-        file = upnp->setCurFile(file.c_str());
+      const char* usn = it->first.c_str();
+      printf("use device %s\n", usn);
+      render = Upnp::Instance()->getRender(usn);
+      if (cmd == "opensub") {
+        int type = USAVTransport;
+        int timeout = 3600;
+        if (cmds.size() > 2) {
+          type = std::stoi(cmds[2]);
+          if (cmd.size() > 3)
+            timeout = std::stoi(cmds[3]);
+        }
+        render->subscribe(type, timeout);
       }
-      upnp->openUrl(cur_dev.c_str(), file.c_str());
     }
-    else if (cmd == "seek") {
-      if (cur_dev.empty()) continue;
-      upnp->seek(cur_dev.c_str(), std::stof(cmds[1]));
+    else if (cmd == "close") {
+      if (render) {
+        Upnp::Instance()->close(render->devId());
+        render = nullptr;
+      }
     }
-    else if (cmd == "pause") {
-      if (cur_dev.empty()) continue;
-      upnp->pause(cur_dev.c_str());
-    }
-    else if (cmd == "resume") {
-      if (cur_dev.empty()) continue;
-      upnp->resume(cur_dev.c_str());
-    }
-    else if (cmd == "stop") {
-      if (cur_dev.empty()) continue;
-      upnp->close(cur_dev.c_str());
-    }
-    else if (cmd == "pos") {
-      if (cur_dev.empty()) continue;
-      upnp->getPosition(cur_dev.c_str(), [](int code, AVPositionInfo pos) {
-        printf("goPos %d %f/%f\n", code, pos.relTime, pos.trackDuration);
-      });
-    }
-    else if (cmd == "tinfo") {
-      if (cur_dev.empty()) continue;
-      upnp->getTransportInfo(cur_dev.c_str(), [](int code, TransportInfo info) {
-        printf("goInfo %d %s %s %f\n", code, info.state, info.status, info.speed);
-      });
-    }
-    else if (cmd == "minfo") {
-      if (cur_dev.empty()) continue;
-      upnp->getMediaInfo(cur_dev.c_str());
-    }
-    else if (cmd == "next") {
-      if (cur_dev.empty()) continue;
-      upnp->next(cur_dev.c_str());
-    }
-    else if (cmd == "prev") {
-      if (cur_dev.empty()) continue;
-      upnp->previous(cur_dev.c_str());
-    }
-    else if (cmd == "subscribe" || cmd == "subs") {
-      if (cur_dev.empty()) continue;
+    else if (cmd == "subscribe" || cmd == "sub") {
+      if (!render) continue;
       int type = USAVTransport;
       int timeout = 3600;
       if (cmds.size() > 1) {
@@ -125,26 +97,98 @@ int main(int argc, char* argv[]) {
         if (cmd.size() > 2)
           timeout = std::stoi(cmds[2]);
       }
-      upnp->subscribe(cur_dev.c_str(), type, timeout);
+      render->subscribe(type, timeout);
     }
-    else if (cmd == "unsubscribe" || cmd == "unsubs") {
-      if (cur_dev.empty()) continue;
+    else if (cmd == "unsubscribe" || cmd == "unsub") {
+      if (!render) continue;
       int type = USAVTransport;
       if (cmds.size() > 1) {
         type = std::stoi(cmds[1]);
       }
-      upnp->unsubscribe(cur_dev.c_str(), type);
+      render->unsubscribe(type);
+    }
+    else if (cmd == "cap") {
+      if(render)
+        render->getDeviceCapabilities(nullptr);
+    }
+    else if (cmd == "seturl" || cmd == "url") {
+      if (cmds.size() < 2 || !render) continue;
+      auto file = cmds[1];
+      if (-1 == file.find("://")) {
+        file = upnp->setCurFile(file.c_str());
+      }
+      render->setAVTransportURL(file.c_str());
+    }
+    else if (cmd == "seek") {
+      if (render) render->seek(std::stof(cmds[1]));
+    }
+    else if (cmd == "pause") {
+      if (render) render->pause();
+    }
+    else if (cmd == "resume") {
+      if (render) render->play();
+    }
+    else if (cmd == "play") {
+      float speed = 1.0f;
+      if (cmds.size() > 1)
+        speed = std::stof(cmds[1]);
+      if (render) render->play(speed);
+    }
+    else if (cmd == "pos") {
+      if (!render) continue;
+      render->getPositionInfo([](int code, AVPositionInfo pos) {
+        printf("goPos %d %f/%f\n", code, pos.relTime, pos.trackDuration);
+      });
+    }
+    else if (cmd == "tinfo") {
+      if (!render) continue;
+      render->getTransportInfo([](int code, TransportInfo info) {
+        printf("goInfo %d %s %s %f\n", code, info.state, info.status, info.speed);
+      });
+    }
+    else if (cmd == "minfo") {
+      if (!render) continue;
+      render->getMediaInfo(nullptr);
+    }
+    else if (cmd == "next") {
+      if (render) render->Next(nullptr);
+    }
+    else if (cmd == "prev") {
+      if (render) render->Previous(nullptr);
     }
     else if (cmd == "vol") {
-      if (cur_dev.empty()) continue;
-      upnp->getVolume(cur_dev.c_str(), [](int code, int vol) {
+      if (!render) continue;
+      render->getVolume([](int code, int vol) {
         printf("gotVolume %d %d\n", code, vol);
       });
     }
     else if (cmd == "setVol") {
-      if (cur_dev.empty()) continue;
+      if (!render) continue;
       int val = atoi(cmds[1].c_str());
-      upnp->setVolume(cur_dev.c_str(), val);
+      render->setVolume(val);
+    }
+    else {
+      printf("unknown cmd %s, supported is\n", cmd.c_str());
+      printf(" quit,q \t quit app\n");
+      printf(" search, \t search devices\n");
+      printf(" list,l \t list devices\n");
+      printf(" open,o \t open device\n");
+      printf(" opensub \t open and subscribe device\n");
+      printf(" close \t close device\n");
+      printf(" subscribe,sub \t subscribe device\n");
+      printf(" unsubscribe,unsub \t unsubscribe device\n");
+      printf(" seturl,url \t set url on opened device\n");
+      printf(" seek \t seek player to a pos\n");
+      printf(" pause \t pause player\n");
+      printf(" resume \t resume player\n");
+      printf(" play \t play with speed\n");
+      printf(" pos \t get player pos\n");
+      printf(" tinfo \t get player transport info\n");
+      printf(" minfo \t get player media info\n");
+      printf(" next \t play next file\n");
+      printf(" prev \t play prev file\n");
+      printf(" vol \t get device volume\n");
+      printf(" setVol \t set device volume\n");
     }
   }
   upnp->delListener(&listener);
