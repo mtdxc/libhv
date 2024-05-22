a8c245fb48c7e41bd25fb6de40a778d14d6d12d7
cqm <cqm@97kid.com>
2024-05-22 17:29:11 +0800

    增加发布订阅

--- examples/redis/RedisClient.cpp
+++ examples/redis/RedisClient.cpp
@@ -1,6 +1,7 @@
 #include "RedisClient.h"
 #include <async.h>
 #include <hiredis.h>
+#include "hlog.h"
 #include <adapters/libhv.h>
 
 void debugCallback(redisAsyncContext *c, void *r, void *privdata) {
@@ -14,26 +15,41 @@ void debugCallback(redisAsyncContext *c, void *r, void *privdata) {
 void RedisClient::connectCallback(const redisAsyncContext *c, int status) {
     RedisClient *self = (RedisClient*)c->data;
     if (status != REDIS_OK) {
-        printf("%p connect error: %s\n", c->data, c->errstr);
-        if (self && self->event_) self->event_->onDisconnect(status);
+        hlogi("%p connect error: %s\n", c->data, c->errstr);
+        if (self) self->onDisconnect(status);
     }
     else {
-        printf("%p Connected...\n", c->data);
-        if (self && self->event_) self->event_->onConnect();
+        hlogi("%p Connected...\n", c->data);
+        if (self) self->onConnect();
     }
 }
 
 void RedisClient::disconnectCallback(const redisAsyncContext *c, int status) {
     RedisClient *self = (RedisClient *)c->data;
-    if (self && self->event_) self->event_->onDisconnect(status);
+    if (self) self->onDisconnect(status);
     if (status != REDIS_OK) {
-        printf("Error: %s\n", c->errstr);
-        return;
+        hlogi("%p Error: %s\n", c->data, c->errstr);
     }
-    printf("Disconnected...\n");
+    else {
+        hlogi("%p Disconnected...\n", c->data);
+    }
 }
 
-bool RedisClient::open(const char* host, int port) {
+void RedisClient::onDisconnect(int code) {
+    if (event_) event_->onDisconnect(code);
+    // reconnect
+    if (code) {
+    }
+}
+
+void RedisClient::onConnect() {
+    for (auto it : subsMap) {
+        redisAsyncCommand(ctx_, &subsCallback, nullptr, "subscribe %s", it.first.c_str());
+    }
+    if (event_) event_->onConnect();
+}
+
+bool RedisClient::open(const char *host, int port) {
     if (port <= 0) port = 6379;
     ctx_ = redisAsyncConnect(host, port);
     if (ctx_->err) {
@@ -52,6 +68,10 @@ bool RedisClient::open(const char* host, int port) {
 }
 
 bool RedisClient::close() {
+    if (subs_timer_) {
+        htimer_del(subs_timer_);
+        subs_timer_ = nullptr;
+    }
     if (auto c = ctx_) {
         c->data = nullptr;
         redisAsyncDisconnect(c);
@@ -75,4 +95,78 @@ void RedisClient::get(const char *key, reply_cb_t cb) {
 void RedisClient::set(const char *key, const std::string& value, reply_cb_t cb) {
     if (!ctx_) return;
     redisAsyncCommand(ctx_, &reply::ReplyCB, reply::CbData(cb), "set %s %b", key, value.c_str(), value.size());
+}
+
+
+void RedisClient::publish(const std::string &channel, const std::string &msg) {
+    if (ctx_) {
+        hlogi("publish %s> %s", channel.c_str(), msg.c_str());
+        redisAsyncCommand(ctx_, nullptr, nullptr, "publish %s %s", channel.c_str(), msg.c_str());
+    }
+}
+
+void RedisClient::subsCallback(redisAsyncContext *ac, void *r, void *privdata) {
+    redisReply *reply = (redisReply *)r;
+    if (reply && reply->type == REDIS_REPLY_ARRAY && reply->elements >= 3) {
+        if (reply->element[2]->type == REDIS_REPLY_INTEGER) {
+            hlogi("redis %s %s %lld", reply->element[0]->str, reply->element[1]->str, reply->element[2]->integer);
+            return;
+        }
+
+        std::string channel, msg;
+        if (0 == strcasecmp(reply->element[0]->str, "message")) {
+            channel = reply->element[1]->str;
+            msg.assign(reply->element[2]->str, reply->element[2]->len);
+        }
+        else if (reply->elements == 4 && !strcasecmp(reply->element[0]->str, "pmessage")) {
+            channel = reply->element[2]->str;
+            msg.assign(reply->element[3]->str, reply->element[3]->len);
+        }
+
+        // subsCount++;
+        hlogi("%s %s< %s", reply->element[0]->str, channel.c_str(), msg.c_str());
+        if (auto redis = (RedisClient*)ac->data) {
+            // MS_DEBUG("%s< %s", channel.c_str(), msg.c_str());
+            try {
+                if (redis->event_)
+                    redis->event_->onMessage(channel, msg);
+                MsgFunc cb = redis->getSubsCB(channel);
+                if(cb)
+                    cb(channel, msg);
+            }
+            catch (std::exception &e) {
+                hlogi(">>> subs %s callback %s exception %s", channel.c_str(), msg.c_str(), e.what());
+            }
+        }
+    }
+    else {
+        hlogi("subsCallback error %p", reply);
+    }
+}
+
+void RedisClient::subscribe(const std::string &channel, MsgFunc func) {
+    hlogi("subscribe %s", channel.c_str());
+    subsMap[channel] = func;
+    if (ctx_) {
+        redisAsyncCommand(ctx_, &subsCallback, nullptr, "subscribe %s", channel.c_str());
+    }    
+    if (!subs_timer_) {
+        float timeout = 60;
+        subs_timer_ = htimer_add(
+            loop_, [](htimer_t *timer) { 
+                auto client = (RedisClient*)hevent_userdata(timer);
+                if (client && client->ctx_) redisAsyncCommand(client->ctx_, nullptr, nullptr, "ping");
+            }, 
+            timeout * 1000);
+        hevent_set_userdata(subs_timer_, this);
+        hlogi("start subsChecker with interval %fs", timeout);
+    }
+}
+
+void RedisClient::unsubscribe(const std::string &channel) {
+    hlogi("unsubscribe %s", channel.c_str());
+    subsMap.erase(channel);
+    if (ctx_) {
+        redisAsyncCommand(ctx_, nullptr, nullptr, "unsubscribe %s", channel.c_str());
+    }
 }
--- examples/redis/main.cpp
+++ examples/redis/main.cpp
@@ -1,16 +1,36 @@
 #include  "RedisClient.h"
-#include "EventLoop.h"
+#include "EventLoopThread.h"
+#include <iostream>
+#include <string>
 
 int main(int argc, char** argv) {
-    hloop_t* loop = hloop_new(0);
-    RedisClient conn(loop);
-    conn.open("192.168.4.12", 6379);
+    int port = 6379;
+    const char* host = "192.168.4.12";
+    if (argc > 1) host = argv[1];
+    if (argc > 2) port = atoi(argv[2]);
+
+    hv::EventLoopThread loop;
+    loop.start();
+    RedisClient conn(loop.hloop());
+    conn.open(host, port);
     conn.set("aa", "bb", [&](reply& r) {
         printf("set return %s\n", r.str().c_str());
         conn.get("aa", [](reply& r) {
             printf("get return %s\n", r.str().c_str());
         });
     });
-    hloop_run(loop);
-    hloop_free(&loop);
+    const char* topic = "topic";
+    RedisClient subs(loop.hloop());
+    subs.open(host, port);
+    subs.subscribe(topic, [](const std::string& channel, const std::string& msg) { 
+        std::cout << channel << "> " << msg << std::endl;
+    });
+    while (1) {
+        std::string s;
+        getline(std::cin, s);
+        if (s == "q" || s == "quit") 
+            break;
+        conn.publish(topic, s);
+    }
+    loop.stop();
 }
