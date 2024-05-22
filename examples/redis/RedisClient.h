394f0318b78dd0390072e435ccc27d5ebd989f5a
cqm <cqm@97kid.com>
2024-05-22 17:29:11 +0800

    增加发布订阅

--- examples/redis/RedisClient.h
+++ examples/redis/RedisClient.h
@@ -1,13 +1,16 @@
 #pragma once
+#include <map>
+#include <string>
 #include "reply.h"
 #include "hv/hloop.h"
 class RedisEvent {
 public:
     virtual void onConnect() = 0;
     virtual void onDisconnect(int code) = 0;
-    // subscribe
-    virtual void onMessage(const char* channel, const char* msg);
+    virtual void onMessage(const std::string& channel, const std::string& msg) = 0;
 };
+
+using MsgFunc = std::function<void(const std::string& channel, const std::string& msg)>;
 
 class RedisClient {
     RedisEvent* event_ = nullptr;
@@ -15,8 +18,19 @@ class RedisClient {
     redisAsyncContext* ctx_ = nullptr;
     static void connectCallback(const redisAsyncContext* c, int status);
     static void disconnectCallback(const redisAsyncContext* c, int status);
+    static void subsCallback(redisAsyncContext* ac, void* r, void* privdata);
+    std::map<std::string, MsgFunc> subsMap;
+    htimer_t* subs_timer_ = nullptr;
+    MsgFunc getSubsCB(const std::string& channel) {
+        MsgFunc ret;
+        if (subsMap.count(channel)) ret = subsMap[channel];
+        return ret;
+    }
+    void onConnect();
+    void onDisconnect(int code);
 public:
     RedisClient(hloop_t* loop) : loop_(loop) {}
+    ~RedisClient() { close(); }
     void setEvent(RedisEvent* ev) {event_ = ev;}
 
     bool open(const char* host, int port);
@@ -25,4 +39,8 @@ public:
     void auth(const char* key, const char* value, reply_cb_t cb);
     void get(const char* key, reply_cb_t cb);
     void set(const char* key, const std::string& value, reply_cb_t cb = nullptr);
+    // ·¢²¼¶©ÔÄ
+    void publish(const std::string& channel, const std::string& msg);
+    void subscribe(const std::string& channel, MsgFunc cb);
+    void unsubscribe(const std::string& channel);
 };
