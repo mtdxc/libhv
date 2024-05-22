b0ae4af09bf9a146e338e97170d1226c5dcf326c
cqm <mtdxc@126.com>
2024-05-22 21:08:49 +0800

    支持断线重连

--- examples/redis/main.cpp
+++ examples/redis/main.cpp
@@ -1,5 +1,6 @@
 #include  "RedisClient.h"
 #include "EventLoopThread.h"
+#include "hlog.h"
 #include <iostream>
 #include <string>
 
@@ -8,10 +9,15 @@ int main(int argc, char** argv) {
     const char* host = "192.168.4.12";
     if (argc > 1) host = argv[1];
     if (argc > 2) port = atoi(argv[2]);
+    hlog_set_handler(stdout_logger);
 
     hv::EventLoopThread loop;
     loop.start();
+
     RedisClient conn(loop.hloop());
+    reconn_setting_t reconn;
+    reconn_setting_init(&reconn);
+    conn.setReconnect(&reconn);
     conn.open(host, port);
     conn.set("aa", "bb", [&](reply& r) {
         printf("set return %s\n", r.str().c_str());
@@ -19,12 +25,15 @@ int main(int argc, char** argv) {
             printf("get return %s\n", r.str().c_str());
         });
     });
+
     const char* topic = "topic";
     RedisClient subs(loop.hloop());
+    subs.setReconnect(&reconn);
     subs.open(host, port);
     subs.subscribe(topic, [](const std::string& channel, const std::string& msg) { 
         std::cout << channel << "> " << msg << std::endl;
     });
+    
     while (1) {
         std::string s;
         getline(std::cin, s);
