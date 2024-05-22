e37c333184901d63a164ca2b85a30a114db1cb48
cqm <mtdxc@126.com>
2024-05-22 21:08:49 +0800

    支持断线重连

--- examples/redis/RedisClient.h
+++ examples/redis/RedisClient.h
@@ -2,7 +2,7 @@
 #include <map>
 #include <string>
 #include "reply.h"
-#include "hv/hloop.h"
+#include "hloop.h"
 class RedisEvent {
 public:
     virtual void onConnect() = 0;
@@ -16,22 +16,39 @@ class RedisClient {
     RedisEvent* event_ = nullptr;
     hloop_t* loop_ = nullptr;
     redisAsyncContext* ctx_ = nullptr;
+    std::string host_;
+    int port_ = 6379;
+    int connect_timeout = 10000;
+    reconn_setting_t* reconn_setting = nullptr;
+    void onConnect();
+    void onDisconnect(int code);
+    int startReconnect();
+    int startConnect();
+
     static void connectCallback(const redisAsyncContext* c, int status);
     static void disconnectCallback(const redisAsyncContext* c, int status);
     static void subsCallback(redisAsyncContext* ac, void* r, void* privdata);
-    std::map<std::string, MsgFunc> subsMap;
+
     htimer_t* subs_timer_ = nullptr;
+    std::map<std::string, MsgFunc> subsMap;
+    void checkSubsTimer();
     MsgFunc getSubsCB(const std::string& channel) {
         MsgFunc ret;
         if (subsMap.count(channel)) ret = subsMap[channel];
         return ret;
     }
-    void onConnect();
-    void onDisconnect(int code);
 public:
     RedisClient(hloop_t* loop) : loop_(loop) {}
-    ~RedisClient() { close(); }
+    ~RedisClient();
     void setEvent(RedisEvent* ev) {event_ = ev;}
+    void setConnectTimeout(int ms) {
+        connect_timeout = ms;
+    }
+
+    void setReconnect(reconn_setting_t* setting);
+    bool isReconnect() {
+        return reconn_setting && reconn_setting->cur_retry_cnt > 0;
+    }
 
     bool open(const char* host, int port);
     bool close();
@@ -39,7 +56,8 @@ public:
     void auth(const char* key, const char* value, reply_cb_t cb);
     void get(const char* key, reply_cb_t cb);
     void set(const char* key, const std::string& value, reply_cb_t cb = nullptr);
-    // ·¢²¼¶©ÔÄ
+
+    // pub sub
     void publish(const std::string& channel, const std::string& msg);
     void subscribe(const std::string& channel, MsgFunc cb);
     void unsubscribe(const std::string& channel);
