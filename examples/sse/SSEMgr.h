2b11cfbc2c7f672639d3ba1ab4b87fba414ce350
mtdxc <mtdxc@126.com>
2025-05-29 22:19:21 +0800

    fixed build add code framework
    ollama 只能用localhost域名访问，否则会出403

--- examples/sse/SSEMgr.h
+++ examples/sse/SSEMgr.h
@@ -7,14 +7,14 @@ class SSEMgr
 public:
 	static SSEMgr& instance();
 
-	void add(LONGLONG id, HttpContextPtr ctx);
+	void add(int64_t id, HttpContextPtr ctx);
 	void broadcast(const std::string& data);
 	bool empty();
 private:
 	SSEMgr();
-	void on_ctx_close(LONGLONG id);
+	void on_ctx_close(int64_t id);
 private:
 	std::mutex m_mtx;
-	std::unordered_map<LONGLONG, HttpContextPtr> m_id_ctxs;
+	std::unordered_map<int64_t, HttpContextPtr> m_id_ctxs;
 };
 
